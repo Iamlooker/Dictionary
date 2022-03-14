583731472d560f4389e591dbe677805469502648
LooKeR <mohit2002ss@gmail.com>
2022-03-14 11:58:59 +0530

    Release v0.2

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -2,6 +2,6 @@ object Android {
     const val appId = "com.looker.dictionary"
     const val minSdk = 21
     const val targetSdk = 32
-    const val versionCode = 1
-    const val versionName = "First Try Baby!!!!"
+    const val versionCode = 2
+    const val versionName = "v0.2"
 }
