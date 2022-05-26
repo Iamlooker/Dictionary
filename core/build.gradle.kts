ea4fd19da94f7572b4615e556c3bb9b11f859a72
Iamlooker <mohit2002ss@gmail.com>
2022-05-27 00:30:39 +0530

    Cleanup

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,24 +4,27 @@ plugins {
 }
 
 android {
-	compileSdk = 32
+	compileSdk = Android.targetSdk
 
 	defaultConfig {
-		minSdk = 21
-		targetSdk = 32
+		minSdk = Android.minSdk
+		targetSdk = Android.targetSdk
 
-		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		consumerProguardFiles("consumer-rules.pro")
 	}
 
 	buildTypes {
 		release {
-			isMinifyEnabled = false
+			isMinifyEnabled = true
 			proguardFiles(
 				getDefaultProguardFile("proguard-android-optimize.txt"),
 				"proguard-rules.pro"
 			)
 		}
+	}
+	buildFeatures {
+		buildConfig = false
+		resValues = false
 	}
 	compileOptions {
 		sourceCompatibility = JavaVersion.VERSION_1_8
@@ -30,14 +33,4 @@ android {
 	kotlinOptions {
 		jvmTarget = "1.8"
 	}
-}
-
-dependencies {
-
-	implementation("androidx.core:core-ktx:1.7.0")
-	implementation("androidx.appcompat:appcompat:1.4.1")
-	implementation("com.google.android.material:material:1.6.0")
-	testImplementation("junit:junit:4.13.2")
-	androidTestImplementation("androidx.test.ext:junit:1.1.3")
-	androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
