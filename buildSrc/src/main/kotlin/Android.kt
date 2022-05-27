5b670e038c11db7a697256d94b952318d4c85607
Iamlooker <mohit2002ss@gmail.com>
2022-05-27 15:15:49 +0530

    Release v0.3

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -2,6 +2,6 @@ object Android {
     const val appId = "com.looker.dictionary"
     const val minSdk = 21
     const val targetSdk = 32
-    const val versionCode = 2
-    const val versionName = "v0.2"
+    const val versionCode = 3
+    const val versionName = "v0.3"
 }
